6f10d843259bbe8aca846ef27b52e90ed393ff99
matinraayai <raayaiardani.m@northeastern.edu>
2020-12-06 18:12:30 -0500

    Bug fix for custom distributons.

--- a2c_ppo_acktr/distributions.py
+++ a2c_ppo_acktr/distributions.py
@@ -1,53 +1,49 @@
-import math
+"""
+Modified standard Pytorch distributions used for sampling actions given states in the policy.
+KL-divergence methods for each distribution is also included.
+"""
+from abc import ABC
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
 from a2c_ppo_acktr.utils import AddBias, init
 
-"""
-Modify standard PyTorch distributions so they are compatible with this code.
-"""
 
-#
-# Standardize distribution interfaces
-#
+class FixedCategorical(torch.distributions.Categorical, ABC):
+    def sample(self, **kwargs):
+        return super().sample(kwargs).unsqueeze(-1)
 
-# Categorical
-class FixedCategorical(torch.distributions.Categorical):
-    def sample(self):
-        return super().sample().unsqueeze(-1)
-
-    def log_probs(self, actions):
-        return (
-            super()
-            .log_prob(actions.squeeze(-1))
-            .view(actions.size(0), -1)
-            .sum(-1)
-            .unsqueeze(-1)
-        )
+    def log_prob(self, actions):
+        return super().log_prob(actions.squeeze(-1)).view(actions.size(0), -1).sum(-1).unsqueeze(-1)
 
     def mode(self):
         return self.probs.argmax(dim=-1, keepdim=True)
 
 
-# Normal
-class FixedNormal(torch.distributions.Normal):
-    def log_probs(self, actions):
+@torch.distributions.register_kl(FixedCategorical, FixedCategorical)
+def calculate_kl(p: FixedCategorical, q: FixedCategorical):
+    return torch.distributions.kl._kl_categorical_categorical(p, q)
+
+
+class FixedNormal(torch.distributions.Normal, ABC):
+    def log_prob(self, actions):
         return super().log_prob(actions).sum(-1, keepdim=True)
 
-    def entrop(self):
-        return super.entropy().sum(-1)
+    def entropy(self):
+        return super().entropy().sum(-1)
 
     def mode(self):
         return self.mean
 
 
-# Bernoulli
-class FixedBernoulli(torch.distributions.Bernoulli):
-    def log_probs(self, actions):
-        return super.log_prob(actions).view(actions.size(0), -1).sum(-1).unsqueeze(-1)
+@torch.distributions.register_kl(FixedNormal, FixedNormal)
+def calculate_kl(p: FixedCategorical, q: FixedCategorical):
+    return torch.distributions.kl._kl_normal_normal(p, q)
+
+
+class FixedBernoulli(torch.distributions.Bernoulli, ABC):
+    def log_prob(self, actions):
+        return super().log_prob(actions).view(actions.size(0), -1).sum(-1).unsqueeze(-1)
 
     def entropy(self):
         return super().entropy().sum(-1)
@@ -55,44 +51,46 @@ class FixedBernoulli(torch.distributions.Bernoulli):
     def mode(self):
         return torch.gt(self.probs, 0.5).float()
 
+@torch.distributions.register_kl(FixedBernoulli, FixedBernoulli)
+def calculate_kl(p: FixedCategorical, q: FixedCategorical):
+    return torch.distributions.kl._kl_bernoulli_bernoulli(p, q)
+
 
 class Categorical(nn.Module):
     def __init__(self, num_inputs, num_outputs):
         super(Categorical, self).__init__()
 
-        init_ = lambda m: init(
-            m,
-            nn.init.orthogonal_,
-            lambda x: nn.init.constant_(x, 0),
-            gain=0.01)
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0), gain=0.01)
 
         self.linear = init_(nn.Linear(num_inputs, num_outputs))
 
+        self.dist = None
+
     def forward(self, x):
         x = self.linear(x)
-        return FixedCategorical(logits=x)
+        self.dist = FixedCategorical(logits=x)
+        return self.dist
 
 
 class DiagGaussian(nn.Module):
     def __init__(self, num_inputs, num_outputs):
         super(DiagGaussian, self).__init__()
 
-        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
-                               constant_(x, 0))
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0))
 
         self.fc_mean = init_(nn.Linear(num_inputs, num_outputs))
         self.logstd = AddBias(torch.zeros(num_outputs))
+        self.dist = None
 
     def forward(self, x):
         action_mean = self.fc_mean(x)
 
         #  An ugly hack for my KFAC implementation.
-        zeros = torch.zeros(action_mean.size())
-        if x.is_cuda:
-            zeros = zeros.cuda()
+        zeros = torch.zeros_like(action_mean.size())
 
         action_logstd = self.logstd(zeros)
-        return FixedNormal(action_mean, action_logstd.exp())
+        self.dist = FixedBernoulli(action_mean, action_logstd.exp())
+        return self.dist
 
 
 class Bernoulli(nn.Module):
@@ -103,7 +101,9 @@ class Bernoulli(nn.Module):
                                constant_(x, 0))
 
         self.linear = init_(nn.Linear(num_inputs, num_outputs))
+        self.dist = None
 
     def forward(self, x):
         x = self.linear(x)
-        return FixedBernoulli(logits=x)
+        self.dist = FixedBernoulli(logits=x)
+        return self.dist

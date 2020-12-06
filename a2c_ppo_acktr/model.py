1a8fce3ca395fcbe04c8e8ecbc8534074af54e76
matinraayai <raayaiardani.m@northeastern.edu>
2020-12-06 17:51:54 -0500

    Modified model.py for a better separation between the actor and critic network and independent gradiant calculation.

--- a2c_ppo_acktr/model.py
+++ a2c_ppo_acktr/model.py
@@ -1,10 +1,9 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-from a2c_ppo_acktr.distributions import Bernoulli, Categorical, DiagGaussian
-from a2c_ppo_acktr.utils import init
+from .distributions import Bernoulli, Categorical, DiagGaussian
+from .utils import init
 
 
 class Flatten(nn.Module):
@@ -19,9 +18,9 @@ class Policy(nn.Module):
             base_kwargs = {}
         if base is None:
             if len(obs_shape) == 3:
-                base = CNNBase
+                base = ActorCriticCNN
             elif len(obs_shape) == 1:
-                base = MLPBase
+                base = ActorCriticMLP
             else:
                 raise NotImplementedError
 
@@ -29,13 +28,13 @@ class Policy(nn.Module):
 
         if action_space.__class__.__name__ == "Discrete":
             num_outputs = action_space.n
-            self.dist = Categorical(self.base.output_size, num_outputs)
+            self.dist_layer = Categorical(self.base.output_size, num_outputs)
         elif action_space.__class__.__name__ == "Box":
             num_outputs = action_space.shape[0]
-            self.dist = DiagGaussian(self.base.output_size, num_outputs)
+            self.dist_layer = DiagGaussian(self.base.output_size, num_outputs)
         elif action_space.__class__.__name__ == "MultiBinary":
             num_outputs = action_space.shape[0]
-            self.dist = Bernoulli(self.base.output_size, num_outputs)
+            self.dist_layer = Bernoulli(self.base.output_size, num_outputs)
         else:
             raise NotImplementedError
 
@@ -48,20 +47,26 @@ class Policy(nn.Module):
         """Size of rnn_hx."""
         return self.base.recurrent_hidden_state_size
 
+    def actor_requires_grad(self, requires_grad):
+        self.base.actor_requires_grad(requires_grad)
+        self.dist_layer.requires_grad_(requires_grad)
+
+    def critic_requires_grad(self, requires_grad):
+        self.base.critic_requires_grad(requires_grad)
+
     def forward(self, inputs, rnn_hxs, masks):
         raise NotImplementedError
 
     def act(self, inputs, rnn_hxs, masks, deterministic=False):
         value, actor_features, rnn_hxs = self.base(inputs, rnn_hxs, masks)
-        dist = self.dist(actor_features)
+        dist = self.dist_layer(actor_features)
 
         if deterministic:
             action = dist.mode()
         else:
             action = dist.sample()
 
-        action_log_probs = dist.log_probs(action)
-        dist_entropy = dist.entropy().mean()
+        action_log_probs = dist.log_prob(action)
 
         return value, action, action_log_probs, rnn_hxs
 
@@ -71,17 +76,38 @@ class Policy(nn.Module):
 
     def evaluate_actions(self, inputs, rnn_hxs, masks, action):
         value, actor_features, rnn_hxs = self.base(inputs, rnn_hxs, masks)
-        dist = self.dist(actor_features)
+        dist = self.dist_layer(actor_features)
 
-        action_log_probs = dist.log_probs(action)
+        action_log_probs = dist.log_prob(action)
         dist_entropy = dist.entropy().mean()
 
         return value, action_log_probs, dist_entropy, rnn_hxs
 
+    def actor_parameters(self):
+        for param in self.base.actor.parameters():
+            yield param
+        for param in self.dist_layer.parameters():
+            yield param
 
-class NNBase(nn.Module):
+    def flat_actor_parameters(self):
+        params = []
+        for param in self.actor_parameters():
+            params.append(param.data.view(-1))
+
+        flat_params = torch.cat(params)
+        return flat_params
+
+    def set_flat_actor_parameters(self, flat_params):
+        prev_ind = 0
+        for param in self.actor_parameters():
+            flat_size = int(np.prod(list(param.size())))
+            param.data.copy_(flat_params[prev_ind:prev_ind + flat_size].view(param.size()))
+            prev_ind += flat_size
+
+
+class ActorCriticBase(nn.Module):
     def __init__(self, recurrent, recurrent_input_size, hidden_size):
-        super(NNBase, self).__init__()
+        super(ActorCriticBase, self).__init__()
 
         self._hidden_size = hidden_size
         self._recurrent = recurrent
@@ -126,11 +152,7 @@ class NNBase(nn.Module):
 
             # Let's figure out which steps in the sequence have a zero for any agent
             # We will always assume t=0 has a zero in it as that makes the logic cleaner
-            has_zeros = ((masks[1:] == 0.0) \
-                            .any(dim=-1)
-                            .nonzero()
-                            .squeeze()
-                            .cpu())
+            has_zeros = ((masks[1:] == 0.0).any(dim=-1).nonzero().squeeze().cpu())
 
             # +1 to correct the masks[1:]
             if has_zeros.dim() == 0:
@@ -166,44 +188,48 @@ class NNBase(nn.Module):
         return x, hxs
 
 
-class CNNBase(NNBase):
+class ActorCriticCNN(ActorCriticBase):
     def __init__(self, num_inputs, recurrent=False, hidden_size=512):
-        super(CNNBase, self).__init__(recurrent, hidden_size, hidden_size)
+        super(ActorCriticCNN, self).__init__(recurrent, hidden_size, hidden_size)
 
-        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
-                               constant_(x, 0), nn.init.calculate_gain('relu'))
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0),
+                               nn.init.calculate_gain('relu'))
 
-        self.main = nn.Sequential(
+        self.actor = nn.Sequential(
             init_(nn.Conv2d(num_inputs, 32, 8, stride=4)), nn.ReLU(),
             init_(nn.Conv2d(32, 64, 4, stride=2)), nn.ReLU(),
             init_(nn.Conv2d(64, 32, 3, stride=1)), nn.ReLU(), Flatten(),
             init_(nn.Linear(32 * 7 * 7, hidden_size)), nn.ReLU())
 
-        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
-                               constant_(x, 0))
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0))
 
-        self.critic_linear = init_(nn.Linear(hidden_size, 1))
+        self.critic = init_(nn.Linear(hidden_size, 1))
 
-        self.train()
+    def actor_requires_grad(self, requires_grad):
+        self.actor.requires_grad_(requires_grad=requires_grad)
+        if self.is_recurrent:
+            self.gru.requires_grad_(requires_grad=requires_grad)
+
+    def critic_requires_grad(self, requires_grad):
+        self.critic.requires_grad_(requires_grad=requires_grad)
 
     def forward(self, inputs, rnn_hxs, masks):
-        x = self.main(inputs / 255.0)
+        x = self.actor(inputs / 255.0)
 
         if self.is_recurrent:
             x, rnn_hxs = self._forward_gru(x, rnn_hxs, masks)
 
-        return self.critic_linear(x), x, rnn_hxs
+        return self.critic(x), x, rnn_hxs
 
 
-class MLPBase(NNBase):
+class ActorCriticMLP(ActorCriticBase):
     def __init__(self, num_inputs, recurrent=False, hidden_size=64):
-        super(MLPBase, self).__init__(recurrent, num_inputs, hidden_size)
+        super(ActorCriticMLP, self).__init__(recurrent, num_inputs, hidden_size)
 
         if recurrent:
             num_inputs = hidden_size
 
-        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
-                               constant_(x, 0), np.sqrt(2))
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0), np.sqrt(2))
 
         self.actor = nn.Sequential(
             init_(nn.Linear(num_inputs, hidden_size)), nn.Tanh(),
@@ -211,19 +237,19 @@ class MLPBase(NNBase):
 
         self.critic = nn.Sequential(
             init_(nn.Linear(num_inputs, hidden_size)), nn.Tanh(),
-            init_(nn.Linear(hidden_size, hidden_size)), nn.Tanh())
+            init_(nn.Linear(hidden_size, hidden_size)), nn.Tanh(),
+            init_(nn.Linear(hidden_size, 1)))
 
-        self.critic_linear = init_(nn.Linear(hidden_size, 1))
+    def actor_requires_grad(self, requires_grad):
+        self.actor.requires_grad_(requires_grad=requires_grad)
+        if self.is_recurrent:
+            self.gru.requires_grad_(requires_grad=requires_grad)
 
-        self.train()
+    def critic_requires_grad(self, requires_grad):
+        self.critic.requires_grad_(requires_grad=requires_grad)
 
-    def forward(self, inputs, rnn_hxs, masks):
-        x = inputs
-
+    def forward(self, x, rnn_hxs, masks):
         if self.is_recurrent:
             x, rnn_hxs = self._forward_gru(x, rnn_hxs, masks)
 
-        hidden_critic = self.critic(x)
-        hidden_actor = self.actor(x)
-
-        return self.critic_linear(hidden_critic), hidden_actor, rnn_hxs
+        return self.critic(x), self.actor(x), rnn_hxs
